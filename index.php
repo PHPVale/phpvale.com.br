1aa346b081c365ea19e281724d6554ba69b468b3
Lucas de Oliveira <94oliveira.lucas@gmail.com>
2016-09-24 15:13:53 -0300

    Initial commt

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html class="no-js" lang="pt-BR"> 
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store">
+  <meta http-equiv="Pragma" content="no-cache, no-store">
+
+  <link rel="icon" type="image/png" href="favicon.png">
+
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" media="screen, print">
+
+  <link rel="stylesheet" type="text/css" href="css/fontes.css" media="screen, print">
+  <link rel="stylesheet" type="text/css" href="css/style.css" media="screen, print">
+
+  <title>PHPVale - Grupo de desenvolvedores PHP do Vale do Paraíba</title>
+</head>
+
+<body>
+  <section>
+    <div class="container">
+      <h1>PHPVale</h1>
+      
+      <p>Bem-vindo ao <a href="http://www.phpvale.com.br" class="link">phpvale.com.br</a>, comunidade de desenvolvedores PHP do Vale do Paraíba. Em breve nosso site estará disponível com artigos, fóruns, eventos e notícias sobre PHP e o universo de desenvolvimento.</p>
+      <p>Quer colaborar? Nos encontre nas redes sociais:</p>
+      <br>
+      <p>
+        <a href="https://www.facebook.com/developers.do.vale" class="social" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
+        <a href="https://twitter.com/phpvale" class="social" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
+        <a href="#" class="social" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a>
+        <a href="https://github.com/PHPVale" class="social" target="_blank"><i class="fa fa-github fa-2x"></i></a>
+        <a href="mailto:contato@phpvale.com.br" class="social"><i class="fa fa-envelope-o fa-2x"></i></a>
+      </p>
+    </div>
+
+    <p class="heart">Made with <i class="fa fa-heart"></i> by phpvale community</p>
+  </section>
+</body>
+</html>
