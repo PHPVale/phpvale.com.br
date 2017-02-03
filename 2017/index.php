b48efe7b2312c42277addaecc6765b776906ec6b
Denilson Raimundo <denilsonraimundo10@gmail.com>
2017-02-03 13:32:22 -0200

    Landing PHPVale event

---
+++ 2017/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html class="no-js" lang="pt-BR"> 
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta http-equiv="Cache-Control" content="no-cache, no-store">
+		<meta http-equiv="Pragma" content="no-cache, no-store">
+
+		<link rel="icon" type="image/png" href="favicon.png">
+
+		<link rel="stylesheet" type="text/css" href="vendor/font-awesome/css/font-awesome.min.css" media="screen, print">
+
+		<link rel="stylesheet" type="text/css" href="css/fonts.css" media="screen, print">
+		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen, print">
+
+		<title>PHPVale - Grupo de desenvolvedores PHP do Vale do Paraíba</title>
+	</head>
+
+	<body>
+		
+		<div class="container">
+			<h1>PHPVale</h1>
+			
+			<h2>1ª edição</h2>
+			<p>29/02/2017 09:00</p>
+			<p>Auditório xxx</p>
+
+			<div class="map">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14626.850459576925!2d-46.67468481180029!3d-23.57876285478677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8e649b93341decc5!2sCampus+S%C3%A3o+Paulo+-+Google+Space!5e0!3m2!1spt-BR!2sbr!4v1486135876328" width="600" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+
+			<section class="subscription">
+				<h2 class="upper">Inscreva-se</h2>
+
+				<form name="" method="POST" action="#">
+					<fieldset>
+						<input type="text" name="ipt-member-name" class="" placeholder="Nome" required>
+						<input type="email" name="ipt-member-email" class="" placeholder="E-mail" required>
+					</fieldset>
+					
+					<input type="submit" class="btn" value="Enviar">
+				</form>
+			</section>
+
+			<section class="support">
+				<h2 class="upper">Apoio</h2>
+
+				<a href="#" class="help"><img src="img/1.png" alt=""></a>
+				<a href="#" class="help"><img src="img/2.png" alt=""></a>
+				<a href="#" class="help"><img src="img/3.png" alt=""></a>
+
+				<p class="container-social">
+					<a href="https://www.facebook.com/developers.do.vale" class="social" target="_blank" title="Facebook PHPVale"><i class="fa fa-facebook fa-2x"></i></a>
+					<a href="https://twitter.com/phpvale" class="social" target="_blank" title="Twitter PHPVale"><i class="fa fa-twitter fa-2x"></i></a>
+					<a href="https://github.com/PHPVale" class="social" target="_blank" title="Github PHPVale"><i class="fa fa-github fa-2x"></i></a>
+					<a href="https://telegram.me/joinchat/CfuMTQoJjUjPQ_39AnE9uQ" class="social" target="_blank" title="Telegram PHPVale"><i class="fa fa-paper-plane fa-2x"></i></a>
+					<a href="mailto:contato@phpvale.com.br" class="social" title="E-mail PHPVale"><i class="fa fa-envelope-o fa-2x"></i></a>
+				</p>
+			</section>
+		</div>
+
+		<p class="heart">Made with <i class="fa fa-heart"></i> by phpvale community</p>
+		
+	</body>
+</html>
